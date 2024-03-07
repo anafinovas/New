9d09b14c49b201cf08c6787ede84874100d0f1b5
Saule <saule.anafinova@gmail.com>
2024-03-07 19:02:26 +0100

    new commit

--- src/main/resources/Main.java
+++ src/main/resources/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Your code here
-        System.out.println("Hello, World!"); // Example print statement
+        System.out.println("Hello, World! We did it!"); // Example print statement
     }
 }
 
